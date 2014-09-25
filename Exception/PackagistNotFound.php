944b150f40780dae3c757fa25b7349400c8df61f
Christian Wielath <info@script-brain.de>
2014-09-25 15:54:16 +0200

    Update PackagistNotFound.php

--- Exception/PackagistNotFound.php
+++ Exception/PackagistNotFound.php
@@ -1,13 +1,13 @@
 <?php
 /**
 *
-* @package symBB
+* @package Symbb
 * @copyright (c) 2013-2014 Christian Wielath
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
-namespace SymBB\ExtensionBundle\Exception;
+namespace Symbb\ExtensionBundle\Exception;
 
 class PackagistNotFound extends \Exception {
     
